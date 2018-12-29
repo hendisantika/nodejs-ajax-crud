36eba302ca68ad279a33386d1df904ea6d1b5d11
Hendi Santika <hendisantika@gmail.com>
2018-12-30 06:12:07 +0700

    #9 - Add index.js file in root folder

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+// settings
+app.set('port', process.env.PORT || 3000);
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// routes
+app.use('/api/products', require('./routes/index'));
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// start the server
+app.listen(app.get('port'), () => {
+    console.log(`server on port ${app.get('port')}`);
+});
